4fb002162bb11d309eb1921c6b4f563a48dc02c1
Awais khatab <42613988+akhatabdev@users.noreply.github.com>
2020-06-27 22:48:01 +0100

    Implemented Java.util ArrayList, removed original list.

--- binary_search.java
+++ binary_search.java
@@ -1,10 +1,11 @@
+import java.util.ArrayList;
 // Java implementation of recursive Binary Search (Decreasing)
 class BinarySearch
 {
 	int step = 0;
     // Returns index of x if it is present in list[l..
     // r], else return -1
-    int binarySearch(int list[], int l, int h, int x) //METHOD
+    int binarySearch(ArrayList<Integer> list, int l, int h, int x) //METHOD
     {
     	++step;
     	
@@ -19,12 +20,12 @@ class BinarySearch
  
             // If the element is present at the 
             // middle itself
-            if (list[mid] == x)
+            if (list.get(mid) == x)
                return mid;
             
             // If element is smaller than mid, then 
             // it can only be present in left sub array
-            if (list[mid] > x)
+            if (list.get(mid) > x)
                return binarySearch(list, l, mid-1, x);
  
             // Else the element can only be present
@@ -39,11 +40,16 @@ class BinarySearch
     // Driver method to test above
     public static void main(String args[])
     {
+        ArrayList<Integer> list = new ArrayList<Integer>(100);
+        for(int x = 0; x < 100; x++){
+            list.add(x);
+    }
+
         BinarySearch ob = new BinarySearch();	//Using the function in this instance
-        int list[] = {1,2,3,4,5,6,7,8,9,10,11};				//List to be searched
-        int n = list.length;					//Length of List 0,1,2,3,4,5,6,7,8,9,10 = 11
+        				//List to be searched
+        int n = list.size();					//Length of List 0,1,2,3,4,5,6,7,8,9,10 = 11
         
-        int x = 7;		// Element to find
+        int x = 75;		// Element to find
         
         int result = ob.binarySearch(list,0,n-1,x);
         
