eec9ca53430e149d4e05c1b00373012543a49ecf
Awais Saif Ali Khatab <awaissaifali@hotmail.co.uk>
2020-07-06 16:33:40 +0100

    Cleaned up code

--- binary_search.java
+++ binary_search.java
@@ -5,38 +5,31 @@ import java.util.Scanner;
 Author: Awais Khatab
 Binary Search with User Input
 Version 1.1.0
-V control: GitHub
+V.Control: GitHub
 */
 
 // Java implementation of recursive Binary Search (Decreasing)
-class BinarySearch
-{
+class BinarySearch {
     int step = 0;
     // Returns index of x if it is present in list[l..
     // r], else return -1
-    int binarySearch(ArrayList<Integer> list, int l, int h, int x)
-    {
+    int binarySearch(ArrayList<Integer> list, int l, int h, int x) {
     	++step;
-    	
     	System.out.println("Step: " + step);
     	System.out.println("Low: " + l);
     	System.out.println("High: " + h);
     	System.out.println(" ");
     	
-        if (h >= l)
-        {
+        if (h >= l) {
             int mid = l + (h - l)/2;
- 
             // If the element is present at the 
             // middle itself
             if (list.get(mid) == x)
                return mid;
-            
             // If element is smaller than mid, then 
             // it can only be present in left sub array
             if (list.get(mid) > x)
                return binarySearch(list, l, mid-1, x);
- 
             // Else the element can only be present
             // in right sub array
             return binarySearch(list, mid+1, h, x);
@@ -47,8 +40,7 @@ class BinarySearch
     }
 
     // Driver method to test above
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         Scanner userInputArray = new Scanner(System.in);
         //SYSTEM MESSAGES AND USER INPUT
         System.out.println("Enter the size of the Array: ");
@@ -65,11 +57,10 @@ class BinarySearch
         }
 
         BinarySearch obj = new BinarySearch();	//Using the function in this instance
-        				//List to be searched
-        int n = list.size();					//Length of List 0,1,2,3,4,5,6,7,8,9,10 = 11
-
-        int x = findX;		// Element to find
-        
+        //List to be searched, User Input size();
+        int n = list.size();
+        // Element to find
+        int x = findX;
         int result = obj.binarySearch(list,0,n-1,x);
         
         if (result == -1)
