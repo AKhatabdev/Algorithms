4d72d438b2ec2fbc27ff881d138308d74d90da79
Awais khatab <akhatabdev@users.noreply.github.com>
2020-07-24 20:13:52 +0100

    - HashMap Update

---
+++ hashmap.java
@@ -0,0 +1,22 @@
+/*
+Author: Awais Khatab
+App: Binary Search with User Input
+Version: 1.4
+All rights reserved @Awais Khatab
+*/
+//HashMap solution to frist non repeating character given within an array of characters
+// Example Array = {a,a,a,b,b,b,c,c,c,d,e,e,e,f,f,f}
+// O(2n) simplified O(n)
+import java.util.
+class hashmap{
+    char firstNonRepeatingCharacter(String s){
+        HashMap<Character, Integer> charCounts = new HashMap<>();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(charCounts.containsKey(c)){ charCounts.put(c, charCounts.get(c) + 1;}
+            else { charCounts.put(c, 1);}
+
+            for(int i = 0; i < s.length(); i++){ char.c = s.charAt(i);
+                if(charCounts.get(c) == 1) return c;}
+        }
+    }
