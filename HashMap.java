2be76cdd49351954c6da73673d15fa16b5eac956
Awais khatab <42613988+akhatabdev@users.noreply.github.com>
2020-07-24 20:48:13 +0100

    - Update, HashMap not finished

--- HashMap.java
+++ HashMap.java
@@ -7,20 +7,30 @@ All rights reserved @Awais Khatab
 //HashMap solution to frist non repeating character given within an array of characters
 // Example Array = {a,a,a,b,b,b,c,c,c,d,e,e,e,f,f,f}
 // O(2n) simplified O(n)
+
 import java.util.HashMap;
 
-public class hashmap{
-    char firstNonRepeatingCharacter(String s){
-        HashMap<Character, Integer> charCounts = new HashMap<>();
-        for(int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
-            if(charCounts.containsKey(c)){
-                charCounts.put(c, charCounts.get(c) + 1;
-            } else { charCounts.put(c, 1); }
+class hashmap{
+    public static void main(String[] args){
+        String array = "aaaabbbcddddeeeefff";
+        firstNonRepeatingCharacter(array);
+    }
 
-            for(int i = 0; i < s.length(); i++){ char.c = s.charAt(i);
-                if(charCounts.get(c) == 1) {
-                    return c;
+    static void firstNonRepeatingCharacter(String s) {
+        HashMap<Character, Integer> charCounts = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (charCounts.containsKey(c)) {
+                charCounts.put(c, charCounts.get(c) + 1);
+            } else {
+                charCounts.put(c, 1);
+            }
+
+            for (int j = 0; j < s.length(); j++) {
+                c = s.charAt(i);
+                if (charCounts.get(c) == 1) {
+                    System.out.print("First non-repeating char is: " + c);
+                    return;
                 }
             }
         }
--- out/production/Algorithms/README.md
+++ out/production/Algorithms/README.md
@@ -1,1 +1,4 @@
-# Search Algorithms
+# Algorithms
+## Author: Awais Khatab
+### Repo for my different Algorithms
+### Feel free to have a look and use it for examples etc.
