ad777fe3fd83fd1509b5a47d1bab8d5a51917ac1
Awais Saif Ali Khatab <awaissaifali@gmail.com>
2020-07-26 23:59:30 +0100

    - Update hashmap.class - Update binary_search.java

--- hashmap.java
+++ hashmap.java
@@ -3,9 +3,10 @@ Author: Awais Khatab
 App: Binary Search with User Input
 Version: 1.4
 All rights reserved @Awais Khatab
+
+HashMap solution to first non repeating character given within a string of characters
+Big O Notation: O(2n) simplified O(n)
 */
-//HashMap solution to first non repeating character given within an array of characters
-// Big O Notation O(2n) simplified O(n)
 import java.util.HashMap;
 
 public class hashmap{
