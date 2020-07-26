0fbc9c531d5e2144ec90a6423c03b30aee55f451
Awais Saif Ali Khatab <awaissaifali@gmail.com>
2020-07-26 23:55:03 +0100

    - Update hashmap.class

--- hashmap.java
+++ hashmap.java
@@ -4,35 +4,37 @@ App: Binary Search with User Input
 Version: 1.4
 All rights reserved @Awais Khatab
 */
-//HashMap solution to frist non repeating character given within an array of characters
-// Example Array = {a,a,a,b,b,b,c,c,c,d,e,e,e,f,f,f}
-// O(2n) simplified O(n)
+//HashMap solution to first non repeating character given within an array of characters
+// Big O Notation O(2n) simplified O(n)
 import java.util.HashMap;
 
 public class hashmap{
     public static void main(String[] args){
-        String input = "aaaaabbbbcdddeeeffff";
+        String input = "aaaaabbbbccccdddeeefggghhiijjkkllmm";
         firstNonRepeatingCharacter(input);
     }
 
-    static void firstNonRepeatingCharacter(String s) {
-        HashMap<Character, Integer> charCounts;
-        charCounts = new HashMap<>();
+    public static void firstNonRepeatingCharacter(String s) {
+        HashMap<Character, Integer> charCounts = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            System.out.println(i);
-            if (charCounts.containsKey(c)) { charCounts.put(c, charCounts.get(c) + 1);  }
-            else { charCounts.put(c, 1); }
-
-            for (int j = 0; j < s.length(); j++) {
-                c = s.charAt(j);
-                if (1 == charCounts.get(c)) {
-                    System.out.print("First non-repeating char is: " + c);
-
-                }
+            if (charCounts.containsKey(c)) {
+                charCounts.put(c, charCounts.get(c) + 1);
+                System.out.println("Found Key: " + c + " New Value is: " + charCounts.get(c));
             }
-            return;
+            else {
+                charCounts.put(c, 1);
+                System.out.println("Adding Key: " + c + " Inputting Value: 1");
+            }
+        }
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (1 == charCounts.get(c)) {
+                System.out.println("Operation Completed\n");
+                System.out.println("First non-repeating char is: " + c);
+                return;
+            }
         }
     }
 }
