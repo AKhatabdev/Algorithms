199f4383b42c47db8f82ab07815fce8f4738d345
Awais khatab <akhatabdev@users.noreply.github.com>
2020-07-26 23:41:33 +0100

    - Fixed logic in hashmap.class

--- HashMap.java
+++ HashMap.java
@@ -7,31 +7,27 @@ All rights reserved @Awais Khatab
 //HashMap solution to frist non repeating character given within an array of characters
 // Example Array = {a,a,a,b,b,b,c,c,c,d,e,e,e,f,f,f}
 // O(2n) simplified O(n)
-
 import java.util.HashMap;
 
-class hashmap{
+public class hashmap{
     public static void main(String[] args){
-        String array = "aaaabbbcddddeeeefff";
-        firstNonRepeatingCharacter(array);
+        String input = "a,a,a,a,a,b,b,b,b,c,d,d,d,e,e,e,f,f,f,f";
+        firstNonRepeatingCharacter(input);
     }
 
     static void firstNonRepeatingCharacter(String s) {
         HashMap<Character, Integer> charCounts = new HashMap<>();
+
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (charCounts.containsKey(c)) {
-                charCounts.put(c, charCounts.get(c) + 1);
-            } else {
-                charCounts.put(c, 1);
-            }
-
-            for (int j = 0; j < s.length(); j++) {
-                c = s.charAt(i);
-                if (charCounts.get(c) == 1) {
-                    System.out.print("First non-repeating char is: " + c);
-                    return;
-                }
+            if (charCounts.containsKey(c)) { charCounts.put(c, charCounts.get(c) + 1);  }
+            else { charCounts.put(c, 1); }
+        }
+        for (int j = 0; j < s.length(); j++) {
+            char c = s.charAt(j);
+            if (1 == charCounts.get(c)) {
+                System.out.print("First non-repeating char is: " + c);
+                return;
             }
         }
     }
