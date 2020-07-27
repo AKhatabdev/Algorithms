11796bcfaa33b5e1f3025c393c602fdf1db4e5e5
Awais khatab <AKhatabdev@users.noreply.github.com>
2020-07-27 01:15:58 +0100

    Update to Comments

--- HashMap.java
+++ HashMap.java
@@ -1,7 +1,7 @@
 /*
 Author: Awais Khatab
 Title: HashMap to find first non-repeating character
-Version: 1.1
+Version: 1.2
 All rights reserved @Awais Khatab
 
 HashMap solution to first non repeating character given within a string of characters
