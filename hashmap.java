6db7eae37189dab7a50f48a6f07236f61a991a66
Awais khatab <42613988+montyybojangless@users.noreply.github.com>
2020-08-01 11:47:03 +0100

    - Refactored and Added User Input for characters.

--- hashmap.java
+++ hashmap.java
@@ -8,12 +8,18 @@ HashMap solution to first non repeating character given within a string of chara
 Big O Notation: O(2n) simplified O(n)
 */
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class hashmap{
     public static boolean checkFlag;
     public static void main(String[] args){
-        String input = "aaaaabbbbccccdddeeefggghhiijjkkllmm";
+        System.out.println("This program will take in a 'string' of characters and will check to see which character is the first non-repeating!\nWhen you are prompted, enter in the characters you wish to check, once you are done press ENTER.\n");
+
+        System.out.println("Enter in your characters: ");
+        Scanner userInput = new Scanner(System.in);
+        String input = userInput.next();
         firstNonRepeatingCharacter(input);
+        userInput.close();
     }
 
     public static void firstNonRepeatingCharacter(String s) {
@@ -35,14 +41,12 @@ public class hashmap{
             char c = s.charAt(i);
             if (1 == charCounts.get(c)) {
                 checkFlag = true;
-                System.out.println("Operation Completed\n");
-                System.out.println("First non-repeating char is: " + c);
+                System.out.println("Operation Completed!\n \nFirst non-repeating char is: " + c);
                 return;
             }
         }
         if (!checkFlag){
-            System.out.println("Operation Completed\n");
-            System.out.println("All characters are repeating!");
+            System.out.println("Operation Completed\n \nAll characters are repeating!");
         }
     }
 }
