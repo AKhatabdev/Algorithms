d7e47f4b3b0c5c105df4c9f9ec32d8c4c583efae
Awais khatab <42613988+montyybojangless@users.noreply.github.com>
2020-08-01 11:48:53 +0100

    Update

--- HashMap.java
+++ HashMap.java
@@ -1,25 +1,30 @@
 /*
 Author: Awais Khatab
 Title: HashMap to find first non-repeating character
-Version: 1.2
+Version: 1.3
 All rights reserved @Awais Khatab
 
 HashMap solution to first non repeating character given within a string of characters
 Big O Notation: O(2n) simplified O(n)
 */
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class hashmap{
     public static boolean checkFlag;
     public static void main(String[] args){
-        String input = "aaaaabbbbccccdddeeefggghhiijjkkllmm";
+        System.out.println("This program will take in a 'string' of characters and will check to see which character is the first non-repeating!\nWhen you are prompted, enter in the characters you wish to check, once you are done press ENTER.\n");
+
+        System.out.println("Enter in your characters: ");
+        Scanner userInput = new Scanner(System.in);
+        String input = userInput.next();
         firstNonRepeatingCharacter(input);
+        userInput.close();
     }
 
     public static void firstNonRepeatingCharacter(String s) {
         HashMap<Character, Integer> charCounts = new HashMap<>();
         checkFlag = false;
-
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (charCounts.containsKey(c)) {
@@ -35,14 +40,12 @@ public class hashmap{
             char c = s.charAt(i);
             if (1 == charCounts.get(c)) {
                 checkFlag = true;
-                System.out.println("Operation Completed\n");
-                System.out.println("First non-repeating char is: " + c);
+                System.out.println("Operation Completed!\n \nFirst non-repeating char is: " + c);
                 return;
             }
         }
         if (!checkFlag){
-            System.out.println("Operation Completed\n");
-            System.out.println("All characters are repeating!");
+            System.out.println("Operation Completed\n \nAll characters are repeating!");
         }
     }
 }
